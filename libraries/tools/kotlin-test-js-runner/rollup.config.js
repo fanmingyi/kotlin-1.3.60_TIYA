6d63f5e2fce17b655e6e6a81fc196fb921412da5
范明毅 <fanmingyi@lizhi.fm>
2022-01-18 12:50:50 +0800

    init

---
+++ libraries/tools/kotlin-test-js-runner/rollup.config.js
@@ -0,0 +1,89 @@
+import typescript from 'rollup-plugin-typescript2';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import {terser} from "rollup-plugin-terser";
+
+const pckg = require('./package.json');
+
+export default [
+    {
+        input: './nodejs.ts',
+        output: {
+            file: 'lib/kotlin-test-nodejs-runner.js',
+            format: 'cjs',
+            banner: '#!/usr/bin/env node',
+            sourcemap: true
+        },
+        plugins: plugins()
+    },
+    {
+        input: './nodejs-source-map-support.js',
+        external: ['path', 'fs', 'module'],
+        output: {
+            file: 'lib/kotlin-nodejs-source-map-support.js',
+            format: 'cjs',
+            sourcemap: true
+        },
+        plugins: [
+            nodeResolve({
+                            jsnext: true,
+                            main: true
+                        }),
+            commonjs(),
+            terser({
+                       compress: true,
+                       sourcemap: true
+                   })
+        ]
+    },
+    {
+        input: './karma.ts',
+        output: {
+            file: 'lib/kotlin-test-karma-runner.js',
+            format: 'cjs',
+            sourcemap: true
+        },
+        plugins: plugins()
+    },
+    {
+        input: './karma-kotlin-reporter.js',
+        external: ['path', 'util'],
+        output: {
+            file: 'lib/karma-kotlin-reporter.js',
+            format: 'cjs'
+        }
+    },
+    {
+        input: './mocha-kotlin-reporter.js',
+        external: ['path', 'util'],
+        output: {
+            file: 'lib/mocha-kotlin-reporter.js',
+            format: 'cjs'
+        }
+    }
+]
+
+function plugins() {
+    return [
+        nodeResolve({
+                        jsnext: true,
+                        main: true
+                    }),
+        commonjs(),
+        typescript({
+                       tsconfig: "tsconfig.json"
+                   }),
+        terser({
+                   sourcemap: true,
+                   compress: {
+                       toplevel: true,
+                       global_defs: {
+                           DEBUG: false,
+                           VERSION: pckg.version,
+                           BIN: Object.keys(pckg.bin)[0],
+                           DESCRIPTION: pckg.description
+                       }
+                   }
+               })
+    ]
+}
---
+++ plugins/allopen/allopen-cli/build.gradle.kts
@@ -0,0 +1,44 @@
+
+description = "Kotlin AllOpen Compiler Plugin"
+
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+dependencies {
+    compileOnly(project(":compiler:plugin-api"))
+    compileOnly(project(":compiler:frontend"))
+    compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
+
+    runtime(kotlinStdlib())
+
+    testRuntimeOnly(intellijDep()) {
+        includeJars("guava", rootProject = rootProject)
+    }
+
+    testRuntimeOnly(project(":kotlin-compiler"))
+    Platform[192].orHigher {
+        testRuntimeOnly(intellijDep()) { includeJars("platform-concurrency") }
+    }
+
+    testCompile(project(":compiler:backend"))
+    testCompile(project(":compiler:cli"))
+    testCompile(projectTests(":compiler:tests-common"))
+    testCompile(commonDep("junit:junit"))
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" { projectDefault() }
+}
+
+runtimeJar()
+
+testsJar()
+
+projectTest(parallel = true) {
+    workingDir = rootDir
+}
+
+apply(from = "$rootDir/gradle/kotlinPluginPublication.gradle.kts")
